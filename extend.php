d6c5b767409c51b1e6dddec6d9bcf5e85f56859a
daocatt <daocatt@users.noreply.github.com>
2024-03-29 14:48:59 +0800

    compatible with extension mentions and flags

--- extend.php
+++ extend.php
@@ -1,6 +1,8 @@
 <?php
 
 use Flarum\Extend;
+use Flarum\Api\Serializer\NotificationSerializer;
+use Flarum\Notification\Notification;
 
 $extend = [
 
@@ -15,6 +17,42 @@ $extend = [
 
     (new Extend\Middleware("api"))
         ->add(\Gtdxyz\DiscussionPaginator\Middleware\DiscussionMiddleware::class),
+
+    (new Extend\Conditional())
+        ->whenExtensionEnabled('flarum-mentions', [
+            (new Extend\ApiSerializer(NotificationSerializer::class))
+                ->attribute('content', function ($serializer, Notification $notification) {
+                    if($notification->type == 'postMentioned'){
+                        if(is_array($notification->data) && array_key_exists('replyNumber',$notification->data)){
+                            $data = $notification->data;
+                            $data['replyNumber'] = ceil($data['replyNumber']/20);
+
+                            return $data;
+                        }
+                    }
+                    return $notification->data;
+                })
+                ->attribute('subject', function ($serializer, Notification $notification) {
+                    
+                    if($notification->type == 'userMentioned' && $notification->subject){
+                        $subject = $notification->subject;
+                        $subject->number = ceil($subject->number/20);
+                        return $subject;
+                    }
+                    return $notification->subject;
+                }),
+        ]),
+    (new Extend\Conditional())->whenExtensionEnabled('flarum-flags', [
+        (new Extend\ApiSerializer(Flarum\Flags\Api\Serializer\FlagSerializer::class))
+                ->attribute('post', function ($serializer, Flarum\Flags\Flag $flag) {
+                    if($flag->post){
+                        $data = $flag->post;
+                        $data->number = ceil($data->number/20);
+                        return $data;
+                    }
+                    return $flag->post;
+                })
+    ]),
 ];
 
 return $extend;
